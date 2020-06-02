149c712bde0337580dd96e51d8aae4f89dbbaea6
Tej Lehal <tejlehal13@gmail.com>
2020-06-02 01:27:21 -0400

    added merge sort and working

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ sortingVisualizerApp.init = function () {
         sortingVisualizerApp.sortingAlgo = $('#algorithms option:selected').val();
         sortingVisualizerApp.arrayLength = parseInt($('input[type=number]').val());
         sortingVisualizerApp.resetArray(sortingVisualizerApp.arrayLength);
+        console.log(sortingVisualizerApp.array);
+        sortingVisualizerApp.sortingAlgorithmCheck(sortingVisualizerApp.sortingAlgo);
 
         console.log(sortingVisualizerApp.array);
     });
@@ -19,14 +21,13 @@ sortingVisualizerApp.init = function () {
 // Create a function that checks the algorithm that the user chose.
 sortingVisualizerApp.sortingAlgorithmCheck = function (userAlgoChoice) {
     if (userAlgoChoice === 'mergeSort') {
-
+        sortingVisualizerApp.array = sortingAlgorithms.mergeSort(sortingVisualizerApp.array);
     }
 }
 
-sortingVisualizerApp.generateArrayBar = function (height) {
-    $('.arrayGraph').append(`<div class="arrayBar arrayBar${height}"><div>`);
-    $(`.arrayBar${height}`).css('height', `${height}px`);
-    console.log('height', height);
+sortingVisualizerApp.generateArrayBar = function (height, index) {
+    $('.arrayGraph').append(`<div class="arrayBar arrayBar${index}"><div>`);
+    $(`.arrayBar${index}`).css('height', `${height}px`);
 }
 
 sortingVisualizerApp.resetArray = function (arrayLength) {
@@ -35,9 +36,8 @@ sortingVisualizerApp.resetArray = function (arrayLength) {
     $('.arrayGraph').empty();
     for (let i = 0; i < arrayLength; i++) {
         randomNumber = sortingVisualizerApp.randomNumber();
-        console.log(randomNumber);
         sortingVisualizerApp.array.push(randomNumber);
-        sortingVisualizerApp.generateArrayBar(randomNumber);
+        sortingVisualizerApp.generateArrayBar(randomNumber, i);
     }
 }
 // Create a method that generates a random number
@@ -46,8 +46,31 @@ sortingVisualizerApp.randomNumber = function () {
 }
 
 const sortingAlgorithms = {
-    mergeSort: function () {
-
+    mergeSort: function (unsortedArr) {
+        if (unsortedArr.length <= 1) {
+            return unsortedArr;
+        }
+        let middle = Math.floor(unsortedArr.length / 2);
+        let leftArr = unsortedArr.slice(0, middle);
+        let rightArr = unsortedArr.slice(middle);
+        return sortingAlgorithms.merge(sortingAlgorithms.mergeSort(leftArr), sortingAlgorithms.mergeSort(rightArr))
     },
+    merge: function (left, right) {
+        let results = [];
+        let leftIndex = 0;
+        let rightIndex = 0;
+
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] < right[rightIndex]) {
+                results.push(left[leftIndex]);
+                leftIndex++;
+            } else {
+                results.push(right[rightIndex]);
+                rightIndex++;
+            }
+        }
+        // There will be an extra one that will be left when getting out of the while loop so we need to concat the index
+        return results.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+    }
 }
 $(document).ready(sortingVisualizerApp.init());
