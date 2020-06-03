7781e468a72f9c661635bb7eac33b550066999ea
Tej Lehal <tejlehal13@gmail.com>
2020-06-02 23:21:44 -0400

    added a swapAnimation function and fixing the merge sort to get an argument of animation

--- script.js
+++ script.js
@@ -24,12 +24,13 @@ sortingVisualizerApp.sortingAlgorithmCheck = function (userAlgoChoice) {
         sortingVisualizerApp.array = sortingAlgorithms.mergeSort(sortingVisualizerApp.array);
     }
 }
-
+//  Create a function that generates (appends) the random bar form the graph 
 sortingVisualizerApp.generateArrayBar = function (height, index) {
     $('.arrayGraph').append(`<div class="arrayBar arrayBar${index}"><div>`);
     $(`.arrayBar${index}`).css('height', `${height}px`);
 }
 
+// Create a funcction that resets the array 
 sortingVisualizerApp.resetArray = function (arrayLength) {
     sortingVisualizerApp.array = [];
     let randomNumber = 0;
@@ -45,18 +46,36 @@ sortingVisualizerApp.randomNumber = function () {
     return Math.floor(Math.random() * (400 - 6) + 5);
 }
 
+sortingVisualizerApp.swapBars = function (animation) {
+    for (let i = 0; i < animation.length; i++) {
+        const $arrayBar = $(`.arrayBar${i}`);
+        const isColorChange = i % 3 !== 2;
+        if (isColorChange) {
+            const [barOne, barTwo] = animation[i];
+            console.log('barsOne and Two', barOne, barTwo);
+        }
+    }
+}
+
 const sortingAlgorithms = {
-    mergeSort: function (unsortedArr) {
+    getMergeAnimation: function (unsortedArr) {
+        const animations = [];
         if (unsortedArr.length <= 1) {
             return unsortedArr;
         }
+        const auxillaryArray = unsortedArr.slice();
+        this.mergeSort(unsortedArr, 0, unsortedArr.length, auxillaryArray, animation);
+    },
+    mergeSort: function (array, startIndex, endIndex, auxillaryArray, animations) {
         let middle = Math.floor(unsortedArr.length / 2);
         let leftArr = unsortedArr.slice(0, middle);
         let rightArr = unsortedArr.slice(middle);
+        // console.log('left', leftArr);
+        // console.log('right', rightArr);
         return sortingAlgorithms.merge(sortingAlgorithms.mergeSort(leftArr), sortingAlgorithms.mergeSort(rightArr))
     },
     merge: function (left, right) {
-        let results = [];
+        const results = [];
         let leftIndex = 0;
         let rightIndex = 0;
 
